8377bdb79a5cfcdd7562d54c4c1d8140c4dbd2bd
Naruto <vijay977364@gmail.com>
2023-07-05 16:25:20 +0530

    Updated

--- Queue.py
+++ Queue.py
@@ -1,38 +1,115 @@
-class Queue1:
+# class Queue:
 
-# QUEUE FIFO 
-
-
-
+# # QUEUE FIFO 
+# # PEP --> Python Enhancement Protocol
 
 
-    def __init__(self,qe):
-        self.qe=qe
 
 
-    def push(self,qe):
-        queue.append(qe)
-        print(queue)
 
-    def pp(self,queue):
-        queue.pop(0)
-        print(queue)
+#     def __init__(self,q):
+#         self.q=q
 
-    def top(self,queue):
-        t=queue[0]
-        print(t)
+#     def push(self,val):
+#         self.q.append(val)
+#         print(queue)
 
-print("Let's implement Queue Here...!")
-print("REMEMBER QUEUE --> FIFO ")
+#     def pop_(self):
+#         if len(self.q)>0:
+#             r=queue.pop(0)
+#             print(queue)
+            
+#         else:
+#             ("Empty Queue..!")
+   
+
+        
+
+#     def top(self):
+#         t=queue[0]
+#         print(t)
+
+# print("Let's implement Queue Here...!")
+# print("REMEMBER QUEUE --> FIFO ")
   
 
-queue=[25,15]
-print(queue)
+# queue=[25,15]
+# print(queue)
 
-v=Queue1(queue)
-print("PUSH")
-v.push(23)
-print("POP")
-v.pp(queue)
-print("TOP")
-v.top(queue)
+# v=Queue(queue)
+# print("PUSH")
+# v.push(23)
+
+# print("POP")
+# v.pop_() 
+
+# print("TOP")
+# v.top()
+
+
+# v2=Queue(queue)
+# print("v2")
+# print(queue)
+
+# v2.push(7)
+# v2.pop_()
+# v2.top()
+
+
+
+class Queue:
+
+    def __init__(self,q):
+        self.q = q
+    
+    def push_to_queue(self,val):
+        print("Pushing an element to Queue!")
+        print(val)
+        b = self.q.append(val)
+        return b
+
+    def pop_from_queue(self):
+        print("Popping an Element..!")
+        v = self.q.pop(0)
+        return v
+
+    def top_of_queue(self):
+        print("TOP of the Queue..!")
+
+        i=self.q[0]
+        return i
+
+
+
+print("Let's try to implement Queue Here...!")
+print(" Note : Queue --> FIFO ")
+
+
+
+queue_lst=[2,58,45]
+
+print("Initial Queue elements..!")
+print(queue_lst)
+    
+a=Queue(queue_lst)
+
+a.push_to_queue(11)
+print (queue_lst)
+
+a.pop_from_queue()
+print(queue_lst)
+
+i=a.top_of_queue()
+print(i)
+
+
+
+
+
+
+ 
+
+
+
+
+
--- Stack.py
+++ Stack.py
@@ -1,30 +1,30 @@
-class Demo1:
+# class Demo1:
 
-# Stack LIFO
-# Queue FIFO
+# # Stack LIFO
+# # Queue FIFO
 
-    def __init__(self,se ):
-         self.se=se
+#     def __init__(self,se ):
+#          self.se=se
 
   
          
 
     
         
-    def psh(self,se):
-        stack.append(se)
-        # print(l)
+#     def psh(self,se):
+#         stack.append(se)
+#         # print(l)
 
 
-    def pp(self,stack):
-        stack.pop()
-        print(stack)
+#     def pp(self,stack):
+#         stack.pop()
+#         print(stack)
 
    
 
-    def top(self,stack):
-        i=stack[-1]
-        print(i)
+#     def top(self,stack):
+#         i=stack[-1]
+#         print(i)
 
 
     
@@ -32,38 +32,95 @@ class Demo1:
 
 
 
-print("Let's implement Stack here...!")    
-print(" REMEMBER STACK --> LIFO ")
+# print("Let's implement Stack here...!")    
+# print(" REMEMBER STACK --> LIFO ")
 
 
-stack=[]
+# stack=[]
 
 
 
-y=Demo1(stack)
-y.psh(33)
+# y=Demo1(stack)
+# y.psh(33)
 
-x=Demo1(stack)
-x.psh(51)
-x.psh(33)
-x.psh(91)
-x.psh(77)
-print(stack)
+# x=Demo1(stack)
+# x.psh(51)
+# x.psh(33)
+# x.psh(91)
+# x.psh(77)
+# print(stack)
 
-print("POP")
-x.pp(stack)
-
- 
- 
-print("TOP")
-x.top(stack)
-
+# print("POP")
+# x.pp(stack)
 
  
-
+ 
+# print("TOP")
+# x.top(stack)
 
 
  
+
+
+
+ 
+class Stack:
+
+   
+    def __init__(self,s):
+        self.s = s
+
+    def push_to_stack(self,val):
+        f = self.s.append(val)
+        print("Pushing an Element!" )
+        print(val)
+        return f
+         
+
+    def pop_from_stack(self):
+
+        if len(self.s)>0:
+            print("Poping an Element!")
+            
+            m = self.s.pop()
+            
+        else:
+            print("Empty Stack..!")
+
+        return m
+
+    def top_of_Stack(self):
+
+        print("Top Of The Stack..!")
+
+        if len(self.s)>0:
+            
+            n = self.s[-1]
+            return n
+        else:
+            print("Empty Stack..!")
+
+        
+print("Let's try to implement Stack Here...!")
+print(" Note : Stack --> LIFO ")
+
+
+stack_lst = [ 12,1]
+
+print("Initial Stack elements..!")
+print(stack_lst)
+
+d = Stack(stack_lst)
+d.push_to_stack(51)
+print(stack_lst)
+d.pop_from_stack()
+print(stack_lst)
+j=d.top_of_Stack()
+print(j)
+ 
+
+ 
+
 
 
    
